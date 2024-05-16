361ab47ae1bc970a05adf0de5b7d23481ad42528
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2024-05-16 23:38:45 +0200

    Case TS016185812 LISTING FILES

--- languages/Cobol.groovy
+++ languages/Cobol.groovy
@@ -309,10 +309,10 @@ def createCompileCommand(String buildFile, LogicalFile logicalFile, String membe
 def createSideFileCommand(String buildFile, String member) {
 	String parms = "$member (COBOL ERROR LOUD"
 	// define the MVSExec command to create the side file
-	MVSExec sideFile = new MVSExec().file(buildFile).pgm('IDILANGX').parm(parms)
+	MVSExec sideFile = new MVSExec().file(buildFile).pgm("IDILANGX").parm(parms)
 
 	// add DD statements
-	sideFile.dd(new DDStatement().name("LISTING").dsn("${props.cobol_listDatasets").options('shr'))
+	sideFile.dd(new DDStatement().name("LISTING").dsn("${props.cobol_listDatasets)").options("shr"))
 	sideFile.dd(new DDStatement().name("IDILANGX").dsn("NAZARE.WDEPLOY.DBBBUILD.GENAPP.SIDEFILE").options('shr').output(true).deployType("SIDEFILE"))
 	sideFile.dd(new DDStatement().name("SYSUDUMP").options(props.cobol_printTempOptions))
 
